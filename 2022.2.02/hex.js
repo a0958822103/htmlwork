2f048337b22b92f978b0391e7d9e70a7c5a97fcc
a0958822103 <a0958822103@gmail.com>
2022-02-08 19:04:15 +0800

    2/8

--- 2022.2.02/hex.js
+++ 2022.2.02/hex.js
@@ -1,18 +1,14 @@
 function hex(){
 
-    const colors=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
-    let hex="#";
-    let x=document.getElementById("btn");
+    const colors = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    let hex = "#";
+    let x = document.getElementById("btn");
     
-    for(let i=1;i<7;i++)
+    for(let i=0;i<6;i++)
     {
-        let y=Math.floor(Math.random()*15);
+        let y = Math.floor(Math.random()*15);
         hex+=colors[y];
     }
-     
-        
-   
-  
     document.body.style.backgroundColor=hex;
     document.getElementById("color").innerHTML=hex;
 }
